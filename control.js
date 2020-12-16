31429735c76768334f61cd64f7196cd5114c53ae
vincent-dante <32755874+vincent-dante@users.noreply.github.com>
2020-12-16 16:07:59 +0800

    add Edit form

--- control.js
+++ control.js
@@ -2,14 +2,29 @@ var app = new Vue({
   el: "#app",
   data() {
     return {
-      todosList: []
+      userList: [],
+      formShow: false,
+      name: null,
+      username: null,
+      email: null
     };
+  },
+  methods: {
+    edit: function (userid) {
+      this.formShow = true;
+      let x = this.userList.find(({ id }) => id === userid);
+      //console.log(this.userList[id]);
+      console.log(x);
+      this.name = x.name;
+      this.username = x.username;
+      this.email = x.email;
+    }
   },
   mounted() {
     axios
-      .get("https://jsonplaceholder.typicode.com/todos/")
+      .get("https://jsonplaceholder.typicode.com/users/")
       .then((response) => {
-        this.todosList = [...response.data].slice(0, 10);
+        this.userList = [...response.data].slice(0, 10);
       });
   }
 });
