17b719f381f25da48a99ce2b55c5090cb54d2f04
vincent-dante <32755874+vincent-dante@users.noreply.github.com>
2020-12-16 14:31:06 +0800

    vue todo list

--- control.js
+++ control.js
@@ -1,6 +1,15 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
+  el: "#app",
+  data() {
+    return {
+      todosList: []
+    };
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/")
+      .then((response) => {
+        this.todosList = [...response.data].slice(0, 10);
+      });
   }
-})
+});
